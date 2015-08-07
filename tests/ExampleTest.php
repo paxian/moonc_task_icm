f347eba3a33dc55d391cf38d333530028ed5aa63
Israel Cuautle <israel.cm@hotmail.com>
2015-08-07 19:34:01 +0300

    Reset ExampleTest.php file.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
+        $this->visit('/');
              ->see('Laravel 5');
     }
 }
